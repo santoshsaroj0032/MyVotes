ae2c381b4ddc013872448d6d4e52a8b9dd20b212
SANTOSH SAROJ <118922022+santoshsaroj0032@users.noreply.github.com>
2024-07-02 10:47:53 +0530

    Update jwt.js

--- jwt.js
+++ jwt.js
@@ -9,7 +9,7 @@ const jwtAuthMiddleware = (req, res, next) => {
     // Extract the jwt token from the request headers
     const token = req.headers.authorization.split(' ')[1];
     if(!token) return res.status(401).json({ error:  'Unauthorized' });
-
+ 
     try{
         // Verify the JWT token
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
