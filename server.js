c2ce62b5d4bb2e87f5e43cf24ba3548039f3b7fe
SANTOSH SAROJ <118922022+santoshsaroj0032@users.noreply.github.com>
2024-07-05 00:35:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const candidateRoutes = require('./routes/candidateRoutes');
 app.use('/user', userRoutes);
 app.use('/candidate', candidateRoutes);
 
-
+ 
 app.listen(PORT, ()=>{
     console.log('listening on port 7000');
 })
